1f0c0fe2e3220e45f24493582f63428fbfc26b40
xancia <cminhma@gmail.com>
2026-01-18 07:50:36 -0500

    changed

--- src/components/UnifiedTopBar.tsx
+++ src/components/UnifiedTopBar.tsx
@@ -36,11 +36,7 @@ const UsersIcon = ({ size = 18 }: { size?: number }) => (
   </svg>
 );
 
-const BrandIcon = () => (
-  <svg className="brand-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M22 12h-4l-3 9L9 3l-3 9H2"/>
-  </svg>
-);
+
 
 const getIcon = (iconType: string, size = 18) => {
   switch (iconType) {
